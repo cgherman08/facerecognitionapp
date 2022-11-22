78e834e5413768a01338caf1b291c7853528ad86
cgherman08 <cgherman08@gmail.com>
2022-11-22 19:03:55 +0200

    sign in & sign out & register

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import ParticlesBg from 'particles-bg';
 import Clarifai from 'clarifai';
 import FaceRecognition from './components/FaceRecogntion/FaceRecognition';
 import Navigation from './components/Navigation/navigation';
+import SignIn from './components/SignIn/SignIn';
+import Register from './components/Register/Register';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
@@ -19,25 +21,26 @@ class App extends Component {
       input: '',
       imageURL: '',
       box: {},
+      route: 'signin',
+      isSignedIn: false
     }
   }
 
   calculateFaceLocation = (data) => {
     const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
-    const image = document.getElementById('inputimage');
+    const image = document.getElementById('newface');
     const width = Number(image.width);
-    const height = Number(image.height);
+    const height = Number(image.height); 
     return{
-      leftCol: clarifaiFace.left_col = width,
-      topRow: clarifaiFace.top_row = height,
+      leftCol: clarifaiFace.left_col * width,
+      topRow: clarifaiFace.top_row * height,
       rightCol: width - (clarifaiFace.right_col * width),
       bottomRow: height - (clarifaiFace.bottom_row * height)
     }  
   }
 
-  dislpayFaceBox = (box) => { 
-    console.log(box);
-    this.setState({box: box});
+  dislpayFaceBox = (box) => {   
+    this.setState({ box });
   }
 
   onInputChange = (event) => {
@@ -50,24 +53,45 @@ class App extends Component {
     .predict(
       Clarifai.FACE_DETECT_MODEL, 
       this.state.input)
-    .then(response => this.calculateFaceLocation(response))
+    .then(response => this.dislpayFaceBox(this.calculateFaceLocation(response)))
     .catch(err => console.log(err));
     
     } 
 
-  render() {  
+    onRouteChange = (route) => {
+      if ( route === 'signout') {
+        this.setState({isSignedIn: 'false'})
+      } else if (route === 'home') {
+        this.setState({isSignedIn:'true'})
+      }
+      this.setState({route: route});
+    }
 
-    return (
-      <div className="App">
-       <ParticlesBg className='particles' type="cobweb" bg={true} />
-       <Navigation />
-       <Logo />
-       <Rank />
-       <ImageLinkForm onInputChange={this.onInputChange} onButtonSubmit={this.onButtonSubmit}/>
-       <FaceRecognition box={this.state.box} imageURL={this.state.imageURL} />
-    </div>
-  );
-}
+  render() {  
+    const { imageURL, box, isSignedIn, route } = this.state;
+      return (
+        <div className="App">
+        <ParticlesBg className='particles' type="cobweb" bg={true} />
+        <Navigation isSignedIn={isSignedIn} onRouteChange={this.onRouteChange} />
+        { route === 'home' 
+        ? <div> 
+            <Logo />       
+            <Rank />
+            <ImageLinkForm 
+            onInputChange={this.onInputChange} 
+            onButtonSubmit={this.onButtonSubmit}/>
+            <FaceRecognition box={ box } imageURL={ imageURL } />
+          </div>
+        : (
+            route === 'signin'
+            ? <SignIn onRouteChange={this.onRouteChange} />
+            : <Register onRouteChange={this.onRouteChange}/>
+          )       
+        }
+      </div>
+      
+    );
+  }   
 }
 
 export default App; 
--- src/components/FaceRecogntion/FaceRecognition.js
+++ src/components/FaceRecogntion/FaceRecognition.js
@@ -5,7 +5,7 @@ const FaceRecognition = ({ imageURL, box }) => {
   return (
     <div className='center ma'>
       <div className='absolute mt2'>
-        <img id='inputimage' alt='' src={imageURL} width='500px' heigh='auto'/>
+        <img id='newface' alt='' src={imageURL} width='500px' heigh='auto'/>
         <div className='bounding-box' style={{top: box.topRow, right: box.rightCol, bottom: box.bottomRow, left: box.leftCol}}></div>
       </div>
     </div>
